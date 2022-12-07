391b3b0e99137acf2d838a1b68fbc6afec8569a6
Joshua Scarberry <joshsc63@github.com>
2022-12-06 22:43:27 -0500

    Add test auth service btn & service

--- go_microservice/broker-service/cmd/api/handlers.go
+++ go_microservice/broker-service/cmd/api/handlers.go
@@ -1,10 +1,23 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"errors"
 	"net/http"
 )
 
 // jsonResponse in helpers.go
+
+type requestPayload struct {
+	Action string `json:"action"`
+	Auth AuthPayload `json:"auth, omitempty"`
+}
+
+type AuthPayload struct {
+	Email string `json:"email"`
+	Password string `json:"password"`
+}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := jsonResponse{
@@ -19,4 +32,78 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	//w.Write(out)
 	
 	_ = app.writeJSON(w, http.StatusOK, payload)
+}
+
+func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
+	var requestPayload requestPayload
+	
+	err := app.readJSON(w, r, &requestPayload)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	
+	switch requestPayload.Action {
+	case "auth":
+		app.authenticate(w, requestPayload.Auth)
+	default:
+		app.errorJSON(w, errors.New("unknown action"))
+	}
+}
+
+func (app *Config) authenticate(w http.ResponseWriter, a AuthPayload) {
+	// create json. Send to auth service
+	jsonData, _ := json.MarshalIndent(a, "", "\t")
+	
+	// call the service
+	// URL is docker compose value
+	request, err := http.NewRequest("POST", "http://authentication-service/authenticate", bytes.NewBuffer(jsonData))
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+	
+	client := &http.Client{}
+	
+	response, err := client.Do(request)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}		
+
+	// dont leave response body open
+	defer response.Body.Close()
+	
+	// check we get back the correct status code
+	if response.StatusCode == http.StatusUnauthorized {
+		app.errorJSON(w, errors.New("invalid credentials"))
+		return
+	} else if response.StatusCode != http.StatusAccepted{
+		app.errorJSON(w, errors.New("error calling auth service"))
+		return
+	}
+	
+	//create var to read response.Body
+	var jsonFromService jsonResponse
+	
+	// decode the json from the auth service
+	err = json.NewDecoder(response.Body).Decode(&jsonFromService)
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	// send error JSON back
+	if jsonFromService.Error {
+		app.errorJSON(w, err, http.StatusUnauthorized)
+		return
+	}
+	
+	// VALID LOGIN
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "Authenticated"
+	payload.Data = jsonFromService.Data
+	
+	app.writeJSON(w, http.StatusAccepted, payload)
 }
--- go_microservice/broker-service/cmd/api/routes.go
+++ go_microservice/broker-service/cmd/api/routes.go
@@ -25,5 +25,8 @@ func (app *Config) routes() http.Handler {
 	
 	mux.Post("/", app.Broker)
 	
+	// handle all requests
+	mux.Post("/handle", app.HandleSubmission)
+	
 	return mux
 }
