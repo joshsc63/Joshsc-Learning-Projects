c8f8fbcf6cbd156163ab488a4d2b56a7b4e38591
Joshua Scarberry <joshsc63@github.com>
2022-12-08 13:09:11 -0500

    add web-service init

--- go_microservice/README.md
+++ go_microservice/README.md
@@ -1,12 +1,13 @@
 # Golang Microservices w/ Postgres, MongoDB
 The building blocks to Golang Microservices that includes 
 
-1. UI site to connect the front end to the golang backend
-2. authentication service
-3. A broker service to handle & route requests
-4. A logger service 
-5. Postgres DB for user data
-6. MongoDB for logging messages
+- UI site to connect the front end to the golang backend
+- A broker service to handle & route requests
+- authentication service using PostGres for user data
+- A logger service that sends messages to MongoDB
+- Mail Service for emails
+- Postgres DB for user data
+- MongoDB for logging messages
 
 Services will deploy on a docker container on the local kubernetes cluster. 
 
@@ -27,6 +28,9 @@ To stop the cluster `make down`
 Navigate to `localhost` on browser
 
 ## Components
+
+### Mail Service
+Uses MailHog to simulate a localhost mail box vs a valid web email. See `docker-compose` - Navigate to `localhost:8025`
 
 ### PG Database
 DB runs locally. See dir `/project/db-data` for PG generated files
---
+++ go_microservice/mail-service/cmd/api/helpers.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+)
+
+type jsonResponse struct {
+	Error bool `json:"error"`
+	Message string `json:"message"`
+	Data any `json:"data,omitempty"` //interface
+}
+
+//Read body of request & convert to JSON
+func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	//limitation of json file size upload
+	maxBytes := 1048576 // 1mb
+	
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+	
+	dec := json.NewDecoder(r.Body)
+	err := dec.Decode(data)
+	if err != nil {
+		return err
+	}
+	
+	//only single json value in file we receive
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must have only a single json value")
+	}
+	
+	return nil
+}
+
+// Takes response status & data then writes json reponse to the client
+func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, headers ...http.Header) error {
+	out, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	
+	//check any headers were included as a final paramter for the function
+	if len(headers) > 0 {
+		//add some headers
+		for key, value := range headers[0] {
+			w.Header()[key] = value
+		}
+	}
+	
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status) //int value
+
+	_, err = w.Write(out)
+	if err != nil {
+		return err
+	}
+	
+	return nil
+}
+
+// write error message as json
+func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) error {
+	statusCode := http.StatusBadRequest
+	
+	//if status has been specified
+	if len (status) > 0 {
+		statusCode = status[0]
+	}
+	
+	var payload jsonResponse
+	payload.Error = true
+	payload.Message = err.Error()
+	
+	return app.writeJSON(w, statusCode, payload)
+}
