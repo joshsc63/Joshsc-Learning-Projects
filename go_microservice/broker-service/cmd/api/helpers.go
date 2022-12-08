5668b26f134f89fd30fe441b389ad024a41de584
Joshua Scarberry <joshsc63@github.com>
2022-12-08 15:02:44 -0500

    add mail to broker

--- go_microservice/broker-service/cmd/api/helpers.go
+++ go_microservice/broker-service/cmd/api/helpers.go
@@ -8,30 +8,30 @@ import (
 )
 
 type jsonResponse struct {
-	Error bool `json:"error"`
+	Error   bool   `json:"error"`
 	Message string `json:"message"`
-	Data any `json:"data,omitempty"` //interface
+	Data    any    `json:"data,omitempty"` //interface
 }
 
-//Read body of request & convert to JSON
+// Read body of request & convert to JSON
 func (app *Config) readJSON(w http.ResponseWriter, r *http.Request, data any) error {
 	//limitation of json file size upload
 	maxBytes := 1048576 // 1mb
-	
+
 	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
-	
+
 	dec := json.NewDecoder(r.Body)
 	err := dec.Decode(data)
 	if err != nil {
 		return err
 	}
-	
+
 	//only single json value in file we receive
 	err = dec.Decode(&struct{}{})
 	if err != io.EOF {
 		return errors.New("body must have only a single json value")
 	}
-	
+
 	return nil
 }
 
@@ -41,7 +41,7 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header
 	if err != nil {
 		return err
 	}
-	
+
 	//check any headers were included as a final paramter for the function
 	if len(headers) > 0 {
 		//add some headers
@@ -49,7 +49,7 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header
 			w.Header()[key] = value
 		}
 	}
-	
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status) //int value
 
@@ -57,22 +57,22 @@ func (app *Config) writeJSON(w http.ResponseWriter, status int, data any, header
 	if err != nil {
 		return err
 	}
-	
+
 	return nil
 }
 
 // write error message as json
 func (app *Config) errorJSON(w http.ResponseWriter, err error, status ...int) error {
 	statusCode := http.StatusBadRequest
-	
+
 	//if status has been specified
-	if len (status) > 0 {
+	if len(status) > 0 {
 		statusCode = status[0]
 	}
-	
+
 	var payload jsonResponse
 	payload.Error = true
 	payload.Message = err.Error()
-	
+
 	return app.writeJSON(w, statusCode, payload)
 }
