98b9ae5798635248afc79f2ac7be25c459154352
Joshua Scarberry <joshsc63@github.com>
2022-12-12 20:30:47 -0500

    handler 2 pages

--- go_website/main.go
+++ go_website/main.go
@@ -5,17 +5,47 @@ import (
 	"net/http"
 )
 
+// const cant be changed by application
+const portNumber = ":8080"
+
+// Handler Functions
+// uppercase becomes public/visible outside of package
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "this is the home page")
+
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	sum := addValues(2, 2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is the about page and 2+2 is %d", sum))
+
+}
+
+// starting lowercase makes it private. Can only be called in this package
+// adds two ints & returns sum
+func addValues(x, y int) int {
+	//var sum int
+	//sum = x + y
+	//return sum
+	return x + y
+}
+
 func main() {
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "hello world!")
-		if err != nil {
-			fmt.Println(err)
-		}
+	// HELLO WORLD
+	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	//	n, err := fmt.Fprintf(w, "hello world!")
+	//	if err != nil {
+	//		fmt.Println(err)
+	//	}
 
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+	//	fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
+	//})
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
 	// start web server
-	_ = http.ListenAndServe(":8080", nil)
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+	_ = http.ListenAndServe(portNumber, nil)
 }
