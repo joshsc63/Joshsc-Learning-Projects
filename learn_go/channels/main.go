b40c6343b8abda164d17233d4bbfe96d8a2644d3
Joshua Scarberry <joshsc63@github.com>
2022-12-14 14:53:24 -0500

    routines

--- learn_go/channels/main.go
+++ learn_go/channels/main.go
@@ -16,18 +16,31 @@ func main() {
 		"http://amazon.com",
 	}
 
+	// main channel
+	c := make(chan string) //make channel of type string
+
 	// ignore index w/ _
 	for _, link := range links {
-		checkLink(link)
+		// pass channel to func
+		go checkLink(link, c) // go routine... use go infront of function calls
 	}
+
+	// receive data from channel
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+// Send data to channel:   channel <- 5    (send the value 5 into channel
+// myNumber <- channel : wait for value to be sent into channel.. when returned, assign value to myNumber
+// fmt.Println(<- channel) : wait for value to be sent in channel. When returned, log it out
+
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link) // 2 return vals... resp & error
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down"
 		return // exit function
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "site is up"
 }
